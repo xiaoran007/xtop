58b91d77986eba7dcb3e1b56a7cda6d10ae28d01
xiaoran <xiaoran2495897824@163.com>
2024-10-26 19:04:25 +0800

    Update: new log

--- src/xtop/frontend/gpu.py
+++ src/xtop/frontend/gpu.py
@@ -1,12 +1,20 @@
 import curses
 import time
 from ..backend.gpu import NvidiaGPU
+import os
 
 
-def GPU_UI(stdscr):
+def GPU_UI(stdscr, enable_log=False):
     curses.curs_set(0)
     stdscr.nodelay(True)
     stdscr.timeout(500)
+
+    if enable_log:
+        message = "xtop Terminal UI For GPU (Log Enable)"
+    else:
+        message = "xtop Terminal UI For GPU"
+
+    magic_number = int(time.time())
 
     nvidia_obj = NvidiaGPU()
     nvidia_obj.init()
@@ -17,7 +25,6 @@ def GPU_UI(stdscr):
 
         height, width = stdscr.getmaxyx()
 
-        message = "xtop Terminal UI For GPU"
         stdscr.addstr(0, 0, message)
 
         dynamic_data = f"Time: {time.strftime('%Y/%m/%d, %H:%M:%S')}"
@@ -29,6 +36,14 @@ def GPU_UI(stdscr):
         for i in range(nvidia_obj.gpu_number):
             stdscr.addstr(i+position_base, 0, nvidia_obj.gpus[i].getTitle())
             stdscr.addstr(i+position_base+1, 4, nvidia_obj.gpus[i].getData())
+            if enable_log:
+                dir_path = os.path.expanduser("~/xtop")
+                os.makedirs(dir_path, exist_ok=True)
+                with open(f"{dir_path}/GPU{i}_{magic_number}.csv", "a") as f:
+                    f.write(f"{time.time()}, {nvidia_obj.gpus[i].utilization}\n")
+                stdscr.addstr(i + position_base + 2, 4, f"File log to {dir_path}/GPU{i}_{magic_number}.csv")
+                position_base += 3
+
             position_base += 2
 
         key = stdscr.getch()
--- src/xtop/frontend/npu.py
+++ src/xtop/frontend/npu.py
@@ -1,12 +1,20 @@
 import curses
 import time
 from ..backend.npu import IntelNPU
+import os
 
 
-def NPU_UI(stdscr):
+def NPU_UI(stdscr, enable_log=False):
     curses.curs_set(0)
     stdscr.nodelay(True)
     stdscr.timeout(500)
+
+    if enable_log:
+        message = "xtop Terminal UI For NPU (Log Enable)"
+    else:
+        message = "xtop Terminal UI For NPU"
+
+    magic_number = int(time.time())
 
     intel_obj = IntelNPU()
     intel_obj.init()
@@ -17,7 +25,6 @@ def NPU_UI(stdscr):
 
         height, width = stdscr.getmaxyx()
 
-        message = "xtop Terminal UI For NPU"
         stdscr.addstr(0, 0, message)
 
         dynamic_data = f"Time: {time.strftime('%Y/%m/%d, %H:%M:%S')}"
@@ -29,6 +36,14 @@ def NPU_UI(stdscr):
         for i in range(intel_obj.npu_number):
             stdscr.addstr(i+position_base, 0, intel_obj.npus[i].getTitle())
             stdscr.addstr(i+position_base+1, 4, intel_obj.npus[i].getData())
+            if enable_log:
+                dir_path = os.path.expanduser("~/xtop")
+                os.makedirs(dir_path, exist_ok=True)
+                with open(f"{dir_path}/NPU{i}_{magic_number}.csv", "a") as f:
+                    f.write(f"{time.time()}, {intel_obj.npus[i].utilization}\n")
+                stdscr.addstr(i + position_base + 2, 4, f"File log to {dir_path}/NPU{i}_{magic_number}.csv")
+                position_base += 3
+
             position_base += 2
 
         key = stdscr.getch()
