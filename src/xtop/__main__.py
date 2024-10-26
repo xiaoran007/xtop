733886b24c932c761e78bc9ee00981f28d27903f
xiaoran <xiaoran2495897824@163.com>
2024-10-26 18:25:25 +0800

    Update: entry point

--- src/xtop/__main__.py
+++ src/xtop/__main__.py
@@ -1,49 +1,37 @@
 import curses
-import time
-from .backend.gpu import NvidiaGPU
-
-
-def TUI(stdscr):
-    curses.curs_set(0)
-    stdscr.nodelay(True)
-    stdscr.timeout(500)
-
-    nvidia_obj = NvidiaGPU()
-    nvidia_obj.init()
-    nvidia_obj.update()
-
-    while True:
-        stdscr.clear()
-
-        height, width = stdscr.getmaxyx()
-
-        message = "xtop Terminal UI"
-        stdscr.addstr(0, 0, message)
-
-        dynamic_data = f"Time: {time.strftime('%Y/%m/%d, %H:%M:%S')}"
-        stdscr.addstr(1, 0, dynamic_data)
-
-        nvidia_obj.update()
-
-        position_base = 2
-        for i in range(nvidia_obj.gpu_number):
-            stdscr.addstr(i+position_base, 0, nvidia_obj.gpus[i].getTitle())
-            stdscr.addstr(i+position_base+1, 4, nvidia_obj.gpus[i].getData())
-            position_base += 2
-
-        key = stdscr.getch()
-        if key == ord('q'):
-            break
-
-        stdscr.refresh()
-
-        time.sleep(0.5)
-
-    nvidia_obj.shutdown()
+from .xtopUtil import getOS
+from . import __version__
+import sys
 
 
 def main():
-    curses.wrapper(TUI)
+    if getOS() != "linux":
+        print(f"Only Linux is supported for now. Current OS: {getOS()}")
+        return
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "-g" or sys.argv[1] == "--gpu":
+            from .frontend import GPU_UI
+            curses.wrapper(GPU_UI)
+        elif sys.argv[1] == '-n' or sys.argv[1] == "--npu":
+            from .frontend import NPU_UI
+            curses.wrapper(NPU_UI)
+        elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
+            print_help()
+        else:
+            print("Wrong argument.")
+            print_help()
+    else:
+        print("Wrong argument.")
+        print_help()
+
+
+def print_help():
+    print("Usage: xtop [Option]")
+    print("Options:")
+    print("\t-g, --gpu\tShow GPU information")
+    print("\t-n, --npu\tShow NPU information")
+    print("\t-h, --help\tShow this help message")
+    print(f"xtop version: {__version__}")
 
 
 if __name__ == "__main__":
