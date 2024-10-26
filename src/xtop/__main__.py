b0f6a71e4f0fb5dfaedf40bad87883b5638fd0ab
xiaoran <xiaoran2495897824@163.com>
2024-10-26 19:04:19 +0800

    Update: argparse

--- src/xtop/__main__.py
+++ src/xtop/__main__.py
@@ -2,40 +2,32 @@ import curses
 from .xtopUtil import getOS
 from . import __version__
 import sys
+import argparse
 
 
 def main():
     if getOS() != "linux":
         print(f"Only Linux is supported for now. Current OS: {getOS()}")
         return
-    if len(sys.argv) == 2:
-        if sys.argv[1] == "-g" or sys.argv[1] == "--gpu":
-            from .frontend import GPU_UI
-            curses.wrapper(GPU_UI)
-        elif sys.argv[1] == '-n' or sys.argv[1] == "--npu":
-            from .frontend import NPU_UI
-            curses.wrapper(NPU_UI)
-        elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
-            print_help()
-        else:
-            print("Wrong argument.")
-            print_help()
+
+    parser = argparse.ArgumentParser(prog="xtop", description="xpu information viewer")
+    parser.add_argument("-g", "--gpu", action="store_true", help="Show GPU information")
+    parser.add_argument("-n", "--npu", action="store_true", help="Show NPU information")
+    parser.add_argument("-l", "--log", action="store_true", help="Create a log file")
+    parser.add_argument("-v", "--version", action="version", version=f"xtop version: {__version__}")
+
+    args = parser.parse_args()
+
+    if args.gpu:
+        from .frontend import GPU_UI
+        curses.wrapper(GPU_UI, args.log)
+    elif args.npu:
+        from .frontend import NPU_UI
+        curses.wrapper(NPU_UI, args.log)
     else:
-        print("Wrong argument.")
-        print_help()
-
-
-def print_help():
-    print("Usage: xtop [Option]")
-    print("Options:")
-    print("\t-g, --gpu\tShow GPU information")
-    print("\t-n, --npu\tShow NPU information")
-    print("\t-h, --help\tShow this help message")
-    print(f"xtop version: {__version__}")
+        parser.print_help()
 
 
 if __name__ == "__main__":
     main()
-
-
 
