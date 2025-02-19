40a99b4f336d1f75b0bc39abd7ab6a65226b3f30
xiaoran Mac mini M4 <xiaoran2495897824@163.com>
2025-02-19 22:19:40 +0800

    Update: command line interface

--- src/xtop/__main__.py
+++ src/xtop/__main__.py
@@ -10,10 +10,10 @@ def main():
         print(f"Only Linux and Windows is supported for now. Current OS: {getOS()}")
         return
 
-    parser = argparse.ArgumentParser(prog="xtop", description="xpu information viewer")
-    parser.add_argument("-g", "--gpu", action="store_true", help="Show GPU information")
-    parser.add_argument("-n", "--npu", action="store_true", help="Show NPU information")
-    parser.add_argument("-l", "--log", action="store_true", help="Create a log file")
+    parser = argparse.ArgumentParser(prog="xtop", description="xpu Performance Monitor")
+    parser.add_argument("-g", "--gpu", action="store_true", help="Monitoring the GPU")
+    parser.add_argument("-n", "--npu", action="store_true", help="Monitoring the NPU (Not available on Windows)")
+    parser.add_argument("-l", "--log", action="store_true", help="Create a log file (Experimental)")
     parser.add_argument("-v", "--version", action="version", version=f"xtop version: {__version__}")
 
     args = parser.parse_args()
@@ -22,6 +22,9 @@ def main():
         from .frontend import GPU_UI
         curses.wrapper(GPU_UI, args.log)
     elif args.npu:
+        if getOS() == "windows":
+            print("NPU is not supported on Windows.")
+            sys.exit(1)
         from .frontend import NPU_UI
         curses.wrapper(NPU_UI, args.log)
     else:
